3388dc147d0b3e711350c28aa9af4020df46f2fb
JoseFlores <joseolmedo.florespaniagua@triosstudent.com>
2026-03-12 10:16:20 -0400

    feat: add book app structure

---
+++ src/app/pages/book-add/book-add.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-book-add',
+  imports: [],
+  templateUrl: './book-add.html',
+  styleUrl: './book-add.css',
+})
+export class BookAdd {
+
+}
