e9a7f4de81bb40338ed432d6835e50c3dec0de6a
JoseFlores <joseolmedo.florespaniagua@triosstudent.com>
2026-03-12 10:42:04 -0400

    feat: add application routing and navigation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
-import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+
+import { Routes } from '@angular/router';
+import { BookList } from './pages/book-list/book-list';
+import { BookAdd } from './pages/book-add/book-add';
+
+export const routes: Routes = [
+  // Redirect the default path to the book list page
+  { path: '', redirectTo: 'list', pathMatch: 'full' },
+
+  // Route for displaying all books
+  { path: 'list', component: BookList },
+
+  // Route for adding a new book
+  { path: 'add', component: BookAdd },
+
+  // Redirect unknown routes to the book list page
+  { path: '**', redirectTo: 'list' }
+];
