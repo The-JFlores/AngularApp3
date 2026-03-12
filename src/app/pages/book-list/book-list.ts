8f6b198bb6bb7411632f9b96a27e1a881a6b7a28
JoseFlores <joseolmedo.florespaniagua@triosstudent.com>
2026-03-12 11:04:13 -0400

    Add book list view

--- src/app/pages/book-list/book-list.ts
+++ src/app/pages/book-list/book-list.ts
@@ -1,11 +1,43 @@
-import { Component } from '@angular/core';
+
+
+import { Component, OnInit, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BookService } from '../../services/book';
+import { Book } from '../../models/book';
 
 @Component({
   selector: 'app-book-list',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './book-list.html',
-  styleUrl: './book-list.css',
+  styleUrl: './book-list.css'
 })
-export class BookList {
+export class BookList implements OnInit {
 
+  // Stores the list of books loaded from the backend
+  books: Book[] = [];
+
+  // Stores an error message if the request fails
+  errorMessage: string = '';
+
+  // Inject the service used to retrieve books
+  private bookService = inject(BookService);
+
+  // Load books when the component is initialized
+  ngOnInit(): void {
+    this.loadBooks();
+  }
+
+  // Request the list of books from the backend
+  loadBooks(): void {
+    this.bookService.getBooks().subscribe({
+      next: (data) => {
+        this.books = data;
+        this.errorMessage = '';
+      },
+      error: (error) => {
+        console.error('Error loading books:', error);
+        this.errorMessage = 'Unable to load books at the moment.';
+      }
+    });
+  }
 }
