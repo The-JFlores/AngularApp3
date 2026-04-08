3bf2d65921113d18e82e0632394c6726017241ec
JoseFlores <joseolmedo.florespaniagua@triosstudent.com>
2026-04-08 11:22:51 -0400

    Add authentication routes and navigation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,10 +5,19 @@ import { Routes } from '@angular/router';
 import { BookList } from './pages/book-list/book-list';
 import { BookAdd } from './pages/book-add/book-add';
 import { BookUpdate } from './pages/book-update/book-update';
+import { Register } from './pages/register/register';
+import { Login } from './pages/login/login';
 
 export const routes: Routes = [
-  // Redirect the default path to the book list page
-  { path: '', redirectTo: 'list', pathMatch: 'full' },
+  
+   // Redirect the default path to the login page
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+
+  // Route for user registration
+  { path: 'register', component: Register },
+
+  // Route for user login
+  { path: 'login', component: Login },
 
   // Route for displaying all books
   { path: 'list', component: BookList },
@@ -20,5 +29,5 @@ export const routes: Routes = [
   { path: 'update/:id', component: BookUpdate },
 
   // Redirect unknown routes to the book list page
-  { path: '**', redirectTo: 'list' }
+  { path: '**', redirectTo: 'login' }
 ];
